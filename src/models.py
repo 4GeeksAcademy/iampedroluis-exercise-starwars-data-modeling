ef45fb6890f2767fd806c7a60821ed0bbf0cb5d4
Pedro Luis Gutierrez <83560861+iampedroluis@users.noreply.github.com>
2023-08-17 22:33:33 +0000

    upload code

--- src/models.py
+++ src/models.py
@@ -7,23 +7,63 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+import os
+import sys
+from sqlalchemy import Column, Enum, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship,  declarative_base
+from sqlalchemy import create_engine
+from eralchemy2 import render_er
+
+Base = declarative_base()
+
+
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
+    height = Column(String(250), nullable=False)
+    mass = Column(String(250), nullable=False)
+    hair_color = Column(String(250), nullable=False)
+    skin_color = Column(String(250), nullable=False)
+    eye_color = Column(String(250), nullable=False)
+    birth_year = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)    
+    description = Column(String(250), nullable=False)
+    homeworld = Column(Integer, ForeignKey('planetas.id'))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(250), nullable=False)
+    diameter = Column(String(250), nullable=False)
+    rotation_period = Column(String(250), nullable=False)
+    orbital_period = Column(String(250), nullable=False)
+    gravity = Column(String(250), nullable=False)
+    population = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    terrain = Column(String(250), nullable=False)
+    surface_water = Column(String(250), nullable=False)   
+    description = Column(String(250), nullable=False)
+    habitantes  = relationship('Planetas', backref='user', lazy=True)
+
+     
+
+    
+ 
+    def to_dict(self):
+        return {}
+
+## Draw from SQLAlchemy base
+try:
+    result = render_er(Base, 'diagram.png')
+    print("Success! Check the diagram.png file")
+except Exception as e:
+    print("There was a problem genering the diagram")
+    raise e
+
 
     def to_dict(self):
         return {}
